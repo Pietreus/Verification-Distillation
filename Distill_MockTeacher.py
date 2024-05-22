Fix LGAD gradient computation

Distill_MockTeacher.py
@@ -13,7 +13,6 @@ class StudentModel(nn.Module):
         self.fc2 = nn.Linear(10, 10)
         self.fc3 = nn.Linear(10, 2)
         self.relu = nn.ReLU()
-        self.softmax = nn.Softmax()
 
     def forward(self, x):
         x = self.relu(self.fc1(x))
@@ -27,7 +26,9 @@ if __name__ == "__main__":
     np.random.seed(42)
     dim = 2
     delta_radius = 0.05
-    for frequency in np.linspace(0.06, 0.56, 26):
+
+    for frequency in np.linspace(0.50, 0.56, 1):
+
         student = StudentModel(dim)
         mock_teacher = MockNeuralNetwork(dim, frequency)
         # Generate synthetic data using normal distribution
@@ -35,7 +36,11 @@ if __name__ == "__main__":
         print(f"teacher frequency: {frequency}  "
               f"teacher {delta_radius}-robustness: {mock_teacher.data_robustness(synthetic_data, delta_radius):0.3f}")
 
-        knowledge_distillation(synthetic_data, mock_teacher, student, 1000, 1, True)
+        knowledge_distillation(synthetic_data, mock_teacher, student, 1000, 50, True)
         torch.onnx.export(student, args=(synthetic_data[1]),
                           f=f"models/dim_{dim}_delta_{delta_radius}_frequency_{frequency}.onnx")
         torch.save(student.state_dict(), f"models/dim_{dim}_delta_{delta_radius}_frequency_{frequency}.pt")
+
+        # confidence is just a threshold use softmax of outputs
+        # for distillation reject samples with low confidence
+        # german credit dataset

KD.py
@@ -15,7 +15,7 @@ from RobustMockTeacher import MockNeuralNetwork
 from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter()
 
-def LGAD(x, y_true, y_pred_S, y_pred_T, lambda_CE=1.0, lambda_KL=3.0, lambda_GAD=5.0,
+def LGAD(x, y_true, y_pred_S, y_pred_T, lambda_CE=1.0, lambda_KL=3.0, lambda_GAD=200.0,
          temperature=1.0):
     LCE = torch.nn.CrossEntropyLoss()
     LKL = torch.nn.KLDivLoss(log_target=True)
@@ -23,10 +23,13 @@ def LGAD(x, y_true, y_pred_S, y_pred_T, lambda_CE=1.0, lambda_KL=3.0, lambda_GAD
     KL_loss = temperature ** 2 * LKL(torch.nn.functional.log_softmax(y_pred_S, dim=1),
                                      torch.nn.functional.log_softmax(y_pred_T, dim=1))
 
-    CE_loss_T = -LCE(y_pred_T, y_true)
-    CE_loss_T.backward(retain_graph=True)
-    CE_loss.backward(retain_graph=True)
-    grad_discrepancy = torch.norm(x.grad)
+    CE_loss_T = LCE(y_pred_T, y_true)
+    # CE_loss_T.backward(retain_graph=True)
+    teacher_grad = torch.autograd.grad(CE_loss_T, x, retain_graph=True, create_graph=True)[0]
+    # CE_loss.backward(retain_graph=True)
+    student_grad = torch.autograd.grad(CE_loss, x, retain_graph=True, create_graph=True)[0]
+    grad_discrepancy = torch.norm(teacher_grad-student_grad)
+    # grad_discrepancy = torch.norm(x.grad)
     # print(f"CE:{CE_loss},KL:{KL_loss},GAD:{grad_discrepancy}")
 
     return lambda_CE * CE_loss + lambda_KL * KL_loss + lambda_GAD * grad_discrepancy, CE_loss, KL_loss, grad_discrepancy